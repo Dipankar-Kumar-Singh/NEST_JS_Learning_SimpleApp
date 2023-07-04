88e36722e057a52b8a37eefaaada6cde4bc6905c
Dipankar <dipankarcode@gmail.com>
2023-07-04 22:32:52 +0530

    project setup

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { json } from 'express';
+
+type User = string ;
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get("/users") 
+  getTest() : any {
+    // return this.appService.getTest() ;
+    return "OKKK!!" ;
+  }
+}
